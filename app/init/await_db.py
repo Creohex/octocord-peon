f7a968c513b0591e9865074c9e4885e9757fbf5f
Creohex <paradise.source@gmail.com>
2020-08-07 22:59:36 +0300

    refactor entrypoint, make peon exception-friendly, add env check, etc

---
+++ app/init/await_db.py
@@ -0,0 +1,30 @@
+import sys
+import time
+import traceback
+
+from peon.utils import Mongo
+
+
+retry_limit = 3
+retry_counter = 1
+print("Waiting for db to come online... (%s retries)" % retry_limit)
+
+while True:
+
+    try:
+        client = Mongo().client
+        client.testDB.testColl.insert({"testkey": "testvalue"})
+        client.drop_database("testDB")
+        client.close()
+        print("Successfully connected.")
+        exit(0)
+    except:
+        print("(%s) Couldn't connect to db, retrying..." % retry_counter, flush=True)
+        traceback.print_exc()
+
+        retry_counter += 1
+        if retry_counter > retry_limit:
+            print("Exceded retry limit. Failed to connect to db.", flush=True)
+            exit(1)
+
+        time.sleep(1)
---
+++ app/init/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+ROOT=$(pwd)
+echo "Starting..."
+
+echo "Checking required env variables..."
+python $ROOT/init/check_env.py
+
+if [ "$?" != 0 ]; then
+    echo "Env variable check failed. exiting."; exit 1
+fi
+
+echo "Awaiting db startup..."
+#python $ROOT/init/await_db.py
+
+if [ $? == "1" ]; then
+    echo "Error! DB not available."
+    exit 1
+fi
+
+python $ROOT/init/start_peon.py
+
+echo "App terminated (code=$?)."
