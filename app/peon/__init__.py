32d490144e7b0a26bfb7a616479f844f394983ff
Creohex <paradise.source@gmail.com>
2020-08-10 01:14:22 +0300

    avoid cross-imports

--- app/peon/__init__.py
+++ app/peon/__init__.py
@@ -1,4 +1,3 @@
-from peon.app import Peon
 from peon.commands import (
     Command,
     MentionHandler,
@@ -11,7 +10,6 @@ from peon.db import (
 )
 
 __all__ = [
-    Peon.__name__,
     Command.__name__, MentionHandler.__name__, CommandSet.__name__,
     Database.__name__, MongoObject.__name__, Document.__name__
 ]
