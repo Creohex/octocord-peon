e88fab98161a341e769a38327aea1d2b50448b0a
Creohex <paradise.source@gmail.com>
2020-08-09 21:34:03 +0300

    correctly init namespace shortcuts

--- app/peon/__init__.py
+++ app/peon/__init__.py
@@ -1,3 +1,17 @@
-from .app import utils, Peon
+from peon.app import Peon
+from peon.commands import (
+    Command,
+    MentionHandler,
+    CommandSet,
+)
+from peon.db import (
+    Database,
+    MongoObject,
+    Document,
+)
 
-__all__ = [Peon.__name__, utils.__name__]
+__all__ = [
+    Peon.__name__,
+    Command.__name__, MentionHandler.__name__, CommandSet.__name__,
+    Database.__name__, MongoObject.__name__, Document.__name__
+]
