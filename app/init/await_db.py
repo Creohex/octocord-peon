b55b5ea13048a8d761a65518b78be418867a2c8a
Creohex <paradise.source@gmail.com>
2020-08-10 01:06:12 +0300

    add check_connection, update await_db

--- app/init/await_db.py
+++ app/init/await_db.py
@@ -2,29 +2,24 @@ import sys
 import time
 import traceback
 
-from peon.utils import Mongo
+from peon.db import check_connection
 
 
-retry_limit = 15
-retry_counter = 1
-print("Waiting for db to come online... (%s retries)" % retry_limit)
+def await_db():
+    retry_limit = 10
+    retry_counter = 1
+    print("Waiting for db to come online... (%s retries)" % retry_limit)
 
-while True:
+    while True:
 
-    try:
-        client = Mongo().client
-        client.testDB.testColl.insert({"testkey": "testvalue"})
-        client.drop_database("testDB")
-        client.close()
-        print("Successfully connected.")
-        exit(0)
-    except:
-        print("(%s) Couldn't connect to db, retrying..." % retry_counter, flush=True)
-        traceback.print_exc()
+        if check_connection():
+            print("Database is available.")
+            break
+        else:
+            print("(%s) Couldn't connect to db, retrying..." % retry_counter, flush=True)
 
-        retry_counter += 1
-        if retry_counter > retry_limit:
-            print("Exceded retry limit. Failed to connect to db.", flush=True)
-            exit(1)
+            time.sleep(1)
 
-        time.sleep(2)
+
+if __name__ == "__main__":
+    await_db()
