4885a7326a38cbd569d10960a323449cf383f102
Creohex <paradise.source@gmail.com>
2020-08-11 14:40:40 +0300

    introduce umongo

--- app/peon/db.py
+++ app/peon/db.py
@@ -2,17 +2,10 @@
 
 import time
 
-import pymongo
-
-from collections import MutableMapping
-from pymongo.errors import (
-    AutoReconnect,
-    ConnectionFailure,
-    OperationFailure,
-    WTimeoutError,
-)
+from umongo import Instance, Document, fields, validate
 
 from peon.utils import (
+    get_db,
     get_env_vars,
     ENV_DB_USER,
     ENV_DB_PASS,
@@ -20,110 +13,36 @@ from peon.utils import (
 )
 
 
+env_vars = get_env_vars()
+DB_NAME = "peondb"
+
+instance = Instance(get_db(
+    env_vars[ENV_DB_HOST], env_vars[ENV_DB_USER], env_vars[ENV_DB_PASS], DB_NAME))
+
+
 def check_connection(db_name="conn_check"):
     """Verify that db is accessible."""
 
     try:
-        db = Database(db_name)
-        db["{0}_test_coll".format(db_name)].insert({"bla": "bla"})
-        db.drop()
+        db = get_db(
+            env_vars[ENV_DB_HOST], env_vars[ENV_DB_USER], env_vars[ENV_DB_PASS],
+            "test_db"
+        )
+        db["test_collection"].insert({"test_field": "test_value"})
+        db["test_collection"].drop()
     except:
+        import traceback
+        traceback.print_exc()
         return False
 
     return True
 
 
-class _Collection(object):
-    """Wrapper over pymongo's collection object."""
+@instance.register
+class Message(Document):
+    """Test mongo entity."""
 
-    class CallProxy(object):
-        """Wrapper for collection's methods."""
+    test_field = fields.StringField(required=False)
 
-        def __init__(self, func, reconnect, timeout):
-            self._func = func
-            self._reconnect = reconnect
-            self._timeout = timeout
-
-        def __call__(self, *args, **kwargs):
-
-            tries = self._reconnect
-            error = None
-
-            while True:
-                try:
-                    return self._func(*args, **kwargs)
-                except (ConnectionFailure, AutoReconnect) as exc:
-                    error = exc
-                except WTimeoutError as exc:
-                    error = exc
-                    break
-
-                if tries <= 1:
-                    break
-
-                time.sleep(self._timeout)
-                tries -= 1
-
-            raise error
-
-    def __init__(self, collection, reconnect, timeout):
-        self.__collection = collection
-        self.__reconnect = reconnect
-        self.__timeout = timeout
-
-    def __getattr__(self, name):
-        value = getattr(self.__collection, name)
-        if callable(value):
-            return self.CallProxy(value, self.__reconnect, self.__timeout)
-        else:
-            return value
-
-    def __getitem__(self, name):
-        return self.__collection[name]
-
-    def __repr__(self):
-        return "_Collection({0}, reconnect={1}, timeout={2})".format(
-            self.__collection.name, self.__reconnect, self.__timeout)
-
-
-class Database(object):
-    """Wrapper over pymongo's database object."""
-
-    def __init__(self, name, reconnect=0, timeout=1):
-        """Database wrapper constructor."""
-
-        self.__name = name
-        self.__reconnect = reconnect
-        self.__timeout = timeout
-        self.__env_vars = get_env_vars()
-
-        self.__database = self._connect()[self.__name]
-
-    def _connect(self):
-        """Returns a connection to MongoDB."""
-
-        connection = pymongo.MongoClient("mongodb://{0}:{1}@{2}".format(
-            self.__env_vars[ENV_DB_USER],
-            self.__env_vars[ENV_DB_PASS],
-            self.__env_vars[ENV_DB_HOST]))
-
-        return connection
-
-    def drop(self):
-        """Alias method to self.client.drop_database(name)."""
-
-        self.__database.client.drop_database(self.__name)
-
-    def __getattr__(self, name):
-        value = getattr(self.__database, name)
-        if isinstance(value, pymongo.collection.Collection):
-            return _Collection(value, self.__reconnect, self.__timeout)
-        else:
-            return value
-
-    def __getitem__(self, name):
-        return _Collection(self.__database[name], self.__reconnect, self.__timeout)
-
-    def __repr__(self):
-        return "Database({0}, reconnect={1}, timeout={2})".format(
-            self.__database.name, self.__reconnect, self.__timeout)
+    class Meta:
+        collection_name = "test_collection"
