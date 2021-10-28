fb8a6c28d1d2b644cd814c2d62a4c31249811349
Creohex <paradise.source@gmail.com>
2021-10-28 14:50:56 +0300

    peon: add translitify cmd

--- app/peon/commands.py
+++ app/peon/commands.py
@@ -368,6 +368,20 @@ async def cmd_punto(message, content, **kwargs):
     await reply(message, switched)
 
 
+async def cmd_translitify(message, content, **kwargs):
+    """Attempt to convert content into gibberish translit."""
+
+    char_map = {
+        "а": "a", "б": "b", "в": "v", "г": "g", "д": "d", "е": "e", "ё": "e", "ж": "zh",
+        "з": "z", "и": "i", "й": "y", "к": "k", "л": "l", "м": "m", "н": "n", "о": "o",
+        "п": "p", "р": "r", "с": "s", "т": "t", "у": "u", "ф": "f", "х": "h", "ц": "ts",
+        "ч": "ch", "ш": "sh", "щ": "sh'", "ы": "y", "э": "e", "ю": "yu", "я": "ya",
+    }
+
+    translit = "".join([char_map.get(char) or char for char in content.lower()])
+    await reply(message, translit)
+
+
 class BaseCommand():
     """Abstract command class."""
 
