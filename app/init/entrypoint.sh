82a9454de15dc0adfa608641459a27e2e793a499
creohex <paradise.source@gmail.com>
2022-10-03 23:26:26 +0300

    app: refactor entrypoint.sh

--- app/init/entrypoint.sh
+++ app/init/entrypoint.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 set -e
-ROOT=$(pwd)
+
+SCRIPTS=$(pwd)/init
 echo "Starting..."
 
+# checks:
 echo "Checking required env variables..."
-python -u $ROOT/init/check_env.py
+python -u $SCRIPTS/check_env.py
 
 if [ "$?" != 0 ]; then
-    echo "Env variable check failed. exiting."; exit 1
-fi
-
-echo "Awaiting db startup..."
-python -u $ROOT/init/await_db.py
-
-if [ $? == "1" ]; then
-    echo "Error! DB not available."
+    echo "Env variable check failed. exiting."
     exit 1
 fi
 
-python -u $ROOT/init/start_peon.py
+echo "Awaiting db startup..."
+python -u $SCRIPTS/await_db.py
 
+if [ $? == 0 ]; then
+    echo "Error! DB not available, exiting..."
+    exit 2
+fi
+
+# application:
+python -u $SCRIPTS/start_peon.py
 echo "App terminated (code=$?)."
