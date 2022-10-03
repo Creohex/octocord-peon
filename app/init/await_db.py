1dceb2705bf3bf59b66bda4f0ed7c8a4c41a3db7
creohex <paradise.source@gmail.com>
2022-10-03 23:06:36 +0300

    app: refactor await_db.py

--- app/init/await_db.py
+++ app/init/await_db.py
@@ -1,24 +1,25 @@
-import sys
-import time
-import traceback
+#!/usr/bin/env python3
 
+import time
 from peon.db import check_connection
 
 
-def await_db():
-    retry_limit = 10
-    retry_counter = 1
-    print("Waiting for db to come online... (%s retries)" % retry_limit)
+def await_db(retries=10):
+    """Attempts to establish connection with database."""
 
-    while True:
+    print("Waiting for db to come online... ({0} retries)".format(retries))
 
+    for counter in range(1, retries + 1):
         if check_connection():
             print("Database is available.")
-            break
-        else:
-            print("(%s) Couldn't connect to db, retrying..." % retry_counter, flush=True)
+            exit(0)
 
-            time.sleep(1)
+        print("({0}) Couldn't connect to db, retrying...".format(counter),
+                flush=True)
+        time.sleep(1)
+    else:
+        print("Failed to connect to database!")
+        exit(1)
 
 
 if __name__ == "__main__":
