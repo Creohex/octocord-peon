840b4dde7d4719c2a75d6244cc367341874e39fe
creohex <paradise.source@gmail.com>
2022-10-07 20:55:37 +0300

    telegram: add constants.py

---
+++ telegram/peon_telegram/constants.py
@@ -0,0 +1,11 @@
+"""Telegram client-related constants."""
+
+
+DEBUG = True
+"""Signals whether to post debug/exception data."""
+
+PREFIX = "/"
+"""Prefix used for basic client commands."""
+
+MSG_MAX_CHARACTERS = 4096
+"""Maximum amount of characters supported in a message."""
