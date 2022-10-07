cfb7d195863e1c276c7aed32dc7d585ae6c485e8
creohex <paradise.source@gmail.com>
2022-10-07 20:55:23 +0300

    common: add exceptions.py

---
+++ common/peon_common/exceptions.py
@@ -0,0 +1,30 @@
+"""Custom exceptions definitions."""
+
+
+class Error(Exception):
+    """Base error."""
+
+    def __init__(self, message=None, *args, **kwargs):
+        args = args
+        kwargs = kwargs
+        message = message.format(*args, **kwargs) if args or kwargs else message
+
+        super(Error, self).__init__(message)
+
+        # if not hasattr(self, "message"):
+        #     self.message = message
+
+
+class CommandError(Error):
+    def __init__(self, message=None):
+        super().__init__(message)
+
+
+class CommandMalformed(CommandError):
+    def __init__(self, message=None):
+        super().__init__(message)
+
+
+class CommandExecutionError(CommandError):
+    def __init__(self, message=None):
+        super().__init__(message)
