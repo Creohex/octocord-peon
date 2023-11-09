036de7388976412d8ab06f538f97d604f2d138a9
creohex <paradise.source@gmail.com>
2023-11-09 19:59:39 +0300

    tools: update update_deps.sh

--- tools/update_deps.sh
+++ tools/update_deps.sh
@@ -7,5 +7,8 @@ libs=("common" "telegram" "discord")
 for lib in "${libs[@]}"; do
     folder="./peon_$lib"
     target_file="./dependencies/deps_$lib.txt"
+    echo "Generating deps for $lib package ($target_file)..."
     poetry export -C "$folder" --output "$target_file" --without-urls
 done
+
+echo "done"
