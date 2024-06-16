cfd569f1c61250ee9c979988a7cb54b3e7e83a67
creohex <paradise.source@gmail.com>
2024-06-17 00:49:10 +0300

    peon_common: add ah.py + tests

---
+++ peon_common/peon_common/ah.py
@@ -0,0 +1,207 @@
+import json
+import re
+import requests
+from dataclasses import dataclass, field
+from datetime import datetime as dt
+from pathlib import Path
+from string import ascii_lowercase
+from typing import Self
+from yarl import URL
+
+import Levenshtein
+from bs4 import BeautifulSoup as bs
+
+from .exceptions import CommandExecutionError
+
+
+@dataclass
+class Price:
+    value: int
+
+    @property
+    def as_string(self):
+        return self.cost_int_to_str(self.value)
+
+    @classmethod
+    def from_string(cls, s: str) -> Self:
+        return cls(cls.cost_str_to_int(s))
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return self.value == other.value
+
+    def __lt__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return self.value < other.value
+
+    def __add__(self, other) -> Self:
+        return Price(self.value + other.value)
+
+    def __sub__(self, other) -> Self:
+        return Price(self.value - other.value)
+
+    def __mul__(self, other) -> Self:
+        return Price(self.value * other.value)
+
+    def __truediv__(self, other) -> Self:
+        return Price(self.value // other.value)
+
+    def __mod__(self, other) -> Self:
+        return Price(self.value % other.value)
+
+    def __repr__(self) -> str:
+        return f"<Price:{self.as_string}>"
+
+    @staticmethod
+    def cost_str_to_int(cost_string: str) -> int:
+        m = re.search(
+            r"((?P<gold>\d+)g\s*)?((?P<silver>\d+)s\s*)?((?P<copper>\d+)c\s*)?",
+            cost_string.strip(),
+        )
+        if not any(m.groupdict().values()):
+            raise ValueError(f"Invalid currency string format: {cost_string}")
+        return sum(
+            [
+                int(m.group("gold") or 0) * 10000,
+                int(m.group("silver") or 0) * 100,
+                int(m.group("copper") or 0),
+            ]
+        )
+
+    @staticmethod
+    def cost_int_to_str(value):
+        return f"{value / 10000:.2f}g"
+
+
+@dataclass
+class Item:
+    id: int
+    name: str
+    price: Price = field(default_factory=lambda: Price(0))
+    last_updated: float = 0.0
+
+    @property
+    def price_readable(self):
+        return self.price.as_string if self.price.value else "?"
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def update(self, obj: Self) -> Self:
+        self.name = obj.name
+        self.price = obj.price
+        self.last_updated = obj.last_updated
+
+
+class AHScraper:
+    LINK_ALPHABET = ascii_lowercase + " 0123456789"
+    LINK_PATTERN = (
+        r"\[(?P<name>[\w\s#:'!\(,\.\s\-\/\&\)]+)\]\([\w\.:\-\/]+\/\?item=(?P<id>\d+)\)"
+    )
+
+    INVALIDATE_AFTER = 86400
+    """Amount of seconds, after which cached items will be considered invalid."""
+
+    def __init__(self, url: URL, items_file: Path) -> Self:
+        self.url = url
+        self.items_file = items_file.absolute()
+        if not self.items_file.is_file():
+            raise Exception(f"No file found! ({self.items_file})")
+
+        self.items = {}
+        self.cache = {}  # id: obj
+
+        if items_file:
+            with open(self.items_file, "r") as f:
+                self.items = json.load(f)
+
+    def build_query_url(self, item: Item) -> URL:
+        words = "".join(c for c in item.name.lower() if c in self.LINK_ALPHABET).split()
+        return self.url / f"{'-'.join(words)}-{str(item.id)}"
+
+    def find_item(self, text: str) -> Item:
+        if not text:
+            return None
+
+        text = text.lower()
+        closest_item = None
+        min_distance = float("inf")
+        partial = [name for name in self.items if text in name]
+
+        for item in partial:
+            distance = Levenshtein.distance(text, item.lower())
+
+            if distance < min_distance:
+                min_distance = distance
+                closest_item = item
+
+        if not closest_item:
+            return None
+
+        return Item(self.items[closest_item], closest_item)
+
+    def _query_auction(self, item: Item) -> str:
+        cached = self.cache.get(item.id)
+        if cached:
+            if (cached.last_updated - dt.now().timestamp()) < self.INVALIDATE_AFTER:
+                item.update(cached)
+                return
+
+        response = requests.get(self.build_query_url(item))
+        if not response.ok:
+            raise CommandExecutionError("Unable to fetch AH data from url")
+
+        soup = bs(response.text, "html.parser")
+        blocks = soup.find_all(
+            lambda tag: tag.name == "td" and "Average Buyout" in tag.get_text()
+        )
+        if len(blocks) != 1:
+            return None
+        cost_block = blocks[0].find_next_sibling()
+        item.price = Price.from_string(cost_block.text)
+        item.last_updated = dt.now().timestamp()
+        self.cache[item.id] = item
+
+    def fetch_prices(self, text: str) -> list:
+        """Try to differentiate specified items and query its prices."""
+
+        items = set()
+        t = text
+
+        for m in re.finditer(self.LINK_PATTERN, text):
+            found = self.find_item(m.group("name"))
+            if found:
+                items.add(found)
+            t = t.replace(m.group(), "")
+
+        if t:
+            for name in t.split(","):
+                found = self.find_item(name)
+                if found:
+                    items.add(found)
+
+        list(map(self._query_auction, items))
+        # for item in items:
+        #     self._query_auction(item)
+        #     item.price = self._query_auction(item)
+
+        return items
+
+    @staticmethod
+    def format_item_prices(items: list[Item]) -> str:
+        if not items:
+            return "nothing found"
+        elif len(items) == 1:
+            item = items.pop()
+            return f"Average price for '{item.name}': {item.price.as_string}"
+        else:
+            return "Avg prices: " + "; ".join(
+                f"{item.name}: {item.price.as_string}" for item in items
+            )
---
+++ peon_common/peon_common/tests/test_ah.py
@@ -0,0 +1,117 @@
+import mock
+import pytest
+import requests
+from pathlib import Path
+from yarl import URL
+
+from peon_common.ah import AHScraper, Item, Price
+
+
+AH_BASE_URL = "https://www.wowauctions.net/auctionHouse"
+NORDNAAR_AH_SCRAPER = AHScraper(f"{AH_BASE_URL}/turtle-wow/nordanaar/mergedAh/",
+                                Path("peon_common/twow_items.json"))
+
+
+TEST_DIR = Path(__file__).resolve(strict=True).parent
+GAME_ITEMS_FILE = TEST_DIR.parent / "twow_items.json"
+AH_REPLY_EXAMPLE_FILE = TEST_DIR / "ah_reply_example.html"
+TEST_URL = URL("https://bla.qwe/")
+
+with open(AH_REPLY_EXAMPLE_FILE, "r") as f:
+    AH_REPLY_EXAMPLE = f.read().replace("\n", "")
+
+
+@pytest.fixture()
+def scraper():
+    ah_scraper = AHScraper(TEST_URL, TEST_DIR.parent / "twow_items.json")
+    response_mock = mock.MagicMock()
+    response_mock.text = lambda: AH_REPLY_EXAMPLE
+
+    with mock.patch.object(requests, "get", return_value=response_mock):
+        yield ah_scraper
+
+
+def test_price():
+    p1 = Price(0)
+    assert p1.value == 0
+    assert p1.as_string == "0.00g"
+    assert p1 == Price(0)
+
+    p2 = Price(152535)
+    assert p2.value == 152535
+    assert p2.as_string == "15.25g"
+    assert p2 > p1
+    assert p2 - Price(2535) == Price(150000)
+
+    p3 = Price.from_string("1g 22s33c")
+    assert p3.value == 12233
+    assert p3.as_string == "1.22g"
+    assert p3.value == Price(12233).value
+
+    with pytest.raises(ValueError):
+        Price.from_string("gibberish")
+
+
+@pytest.mark.parametrize(
+    ("item", "expected_url"),
+    [
+        (Item(0, "item0"), TEST_URL / "item0-0"),
+        (Item(123, "A rather complex 'string' #abc!"), TEST_URL / "a-rather-complex-string-abc-123"),
+    ]
+)
+def test_build_query_url(scraper, item, expected_url):
+    complete_url = scraper.build_query_url(item)
+    assert complete_url == expected_url
+    assert isinstance(complete_url, URL)
+
+
+@pytest.mark.parametrize(
+    ("query", "expected"),
+    [
+        ("", None),
+        ("gibberish", None),
+        ("dream", "dreamfoil"),
+        ("greater int", "elixir of greater intellect"),
+        ("bl", "bleach"),
+        ("copper bar", "copper bar"),
+        ("major mana", "major mana potion"),
+        ("1", "the 1 ring"),
+        ("&", "red & blue mechanostrider"),
+        ("n-", "ginn-su sword"),
+    ],
+)
+def test_find_item(scraper, query, expected):
+    found = scraper.find_item(query)
+
+    if expected:
+        assert found.name == expected
+    else:
+        assert found is None
+
+
+def test_query_auction(scraper):
+    item = Item(61224, "Dreamshared Elixir")
+    assert item.price.value == 0
+
+    price = scraper._query_auction(item)
+    assert price.value == 15900
+
+
+@pytest.mark.parametrize(
+    ("text", "found_items"),
+    [
+        ("", 0),
+        ("gibberish", 0),
+        ("dreamsha", 1),
+        ("[broom](https://database.turtle-wow.org/?item=0)", 1),
+        ("[broom](https://database.turtle-wow.org/?item=0) major rejuv", 2),
+        ("[broom](https://database.turtle-wow.org/?item=0) "
+         "gibberish, major mana pot,"
+         "[snow](https://database.turtle-wow.org/?item=0), limited invul", 4),
+        ("[Eslynn](https://armory.turtle-wow.org/#!/character/Eslynn): !ah elixir, [Le Fishe Au Chocolat](https://database.turtle-wow.org/?item=61666), sand", 3)
+    ],
+)
+def test_fetch_prices(scraper, text, found_items):
+    with mock.patch.object(AHScraper, "_query_auction", return_value=123):
+        items = scraper.fetch_prices(text)
+        assert len(items) == found_items
